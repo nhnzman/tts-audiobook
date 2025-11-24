401cc044fd1eb1fc6fd1d571e818ba269305a87a
KIM GEUNHYUNG <zman@nhn.com>
2025-11-24 17:35:38 +0900

    RaoTTS

---
+++ app/src/main/java/com/lao/tts/MainActivity.kt
@@ -0,0 +1,14 @@
+
+package com.lao.tts
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.lao.tts.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
